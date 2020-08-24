09fb03b41d52e425185022502aee0e8cb92671f5
Harshil Gohil <harshilgohil@MAC-MOHAWK.local>
2020-08-24 13:23:08 -0700

    changes

--- sentiment analyzer/prac.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import numpy as np
-
-with open('sentiment_app/model_disaster_200d_glove.h5','rb') as file:
-    mp = pickle.load(file)
-#clf = pickle.load(open('sentiment_app/model_disaster_200d_glove.h5', 'rb'))
-dir(mp.predict())
-
